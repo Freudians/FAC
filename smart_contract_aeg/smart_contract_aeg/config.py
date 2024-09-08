d450656dd2d4706b2310aadd87b6102321820548
Anthony Du <70033069+Freudians@users.noreply.github.com>
2024-09-08 00:24:53 -0500

    Update config.py

--- smart_contract_aeg/smart_contract_aeg/config.py
+++ smart_contract_aeg/smart_contract_aeg/config.py
@@ -1,4 +1,4 @@
 class Config:
     model = "gpt-3.5-turbo-16k"
 import os
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "REDACTED"
